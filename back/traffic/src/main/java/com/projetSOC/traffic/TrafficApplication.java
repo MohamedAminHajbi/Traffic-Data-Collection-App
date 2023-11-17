f76e145d56aebb04f1cf0710cc8ff7e40251d51c
MohamedAminHajbi <aminhajbi116@gmail.com>
2023-11-17 14:50:59 +0100

    Starting files

---
+++ back/traffic/src/main/java/com/projetSOC/traffic/TrafficApplication.java
@@ -0,0 +1,13 @@
+package com.projetSOC.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficApplication.class, args);
+	}
+
+}
